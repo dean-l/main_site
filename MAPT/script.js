77637e4197f7c1e46ac67aec819a26c0c5027d22
Dean Langton <dean.langton@uq.net.au>
2019-01-21 20:55:15 +1000

    Mapt page added

---
+++ MAPT/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    var imgsToShow = ($(document).width() >= 600) ? 5 : 3; //only show 3 images on mobile
+
+    $('.carousel').slick({
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: imgsToShow,
+        infinite: false,
+        adaptiveHeight: true,
+        asNavFor: '.carousel_text',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: imgsToShow
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+    $('.carousel_text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.carousel'
+    })
+  });
